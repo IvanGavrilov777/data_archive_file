d0e736af072f5124d3cf9b43dbfe60e0e0473607
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2024-11-22 12:26:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
 data "archive_file" "lambda_my_function" {
-  type        = "zip"
+  #type        = "zip"
   #source_file = "${path.module}/vancho.py"
   #output_path = "${path.module}/vancho.zip"
   output_file_mode = "0666"
-  source_file = "vancho.py"
-  output_path = "vancho.zip"
+  #source_file = "vancho.py"
+  #output_path = "vancho.zip"
+
+  type = var.lambda_archive_file_type
+  source_file = var.source_file
+  output_path = var.output_path
+
 }
+
+variable "source_file" {}
+
+variable "output_path" {}
+
+
+variable "lambda_archive_file_type" {}
+
